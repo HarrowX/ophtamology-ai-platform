25fdd4115e109c77cb820b27b0ab0eb66ec91373
harrow <sashok_play@mail.ri>
2025-11-21 17:00:37 +0400

    init base microservices structure

---
+++ ophtamology/src/test/java/xyz/alexandrit/apigateway/OphtamologyTests.java
@@ -0,0 +1,13 @@
+package xyz.alexandrit.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OphtamologyTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
